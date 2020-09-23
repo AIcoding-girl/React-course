6ae9817f8073a0b9859c3687a4c1df9876a5e3da
Skuja <evita.skuja@accenture.com>
2020-09-23 10:15:32 +0300

    added new practice file

---
+++ Vacation-spot-practice/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function MyInfo {
+    return (<div>
+        <h1>
+            Evita
+        </h1>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <ol>
+            <li>Germany</li>
+            <li>Austria</li>
+            <li>Norway</li>
+        </ol>
+    </div>)
+}
+
+ReactDOM.render(
+    <MyInfo />,
+    document.getElementById("root")
+)
