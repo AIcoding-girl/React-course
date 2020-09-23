dff132a9378d9c9fa186ad6feffd9409d67d528c
aicoding-girl <erita13@gmail.com>
2020-09-23 12:56:34 +0300

    file structure part added

---
+++ 07-File-structure/index.js
@@ -0,0 +1,14 @@
+/*
+Set up the React app from scratch
+Inside App, render:
+    1) A Navbar component
+    2) A MainContent component
+    3) A Footer component
+*/
+
+import React from " react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
