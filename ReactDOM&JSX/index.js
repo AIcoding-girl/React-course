7ad404c0997c6a010b91d2fcea55ec17ef88bab1
Skuja <evita.skuja@accenture.com>
2020-09-23 09:19:34 +0300

    added basic structure

---
+++ ReactDOM&JSX/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// JSX
+
+// ReactDOM.render(WHAT DO I WANT TO RENDER, WHERE DO I WANT TO RENDER IT)
+ReactDOM.render(<h1>I am learning!</h1>, document.getElementById("root"))
