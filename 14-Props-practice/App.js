7644ce176aac859fc0c7df9d7fd0273c6e5d0b04
aicoding-girl <erita13@gmail.com>
2020-09-24 19:50:26 +0300

    styling practice added

---
+++ 14-Props-practice/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Joke from "./Joke"
+
+function App() {
+    return (
+        <div>
+            <Joke question="lorem lorem lorem" punchLine="lorumgj hsfe"/>
+            <hr/>
+            <Joke question="lorem lorem lorem" punchLine="lorumgj hsfe"/>
+            <Joke question="lorem lorem lorem" punchLine="lorumgj hsfe"/>
+        </div>
+    )
+}
+
+export default App
