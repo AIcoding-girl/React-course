7e663485fbb33aef03952cf2518593cb16b7346d
aicoding-girl <erita13@gmail.com>
2020-09-27 11:23:17 +0300

    basics of state explained

--- 20-React-state/App.js
+++ 20-React-state/App.js
@@ -1,18 +1,24 @@
 // Props are immutable
 // State needs class based components
 
+// The only way how to pass information from one component to another, is through props
+
 import React from 'react'
 
 class App extends React.Component {
+    // constructor - place where to initialise the values
     constructor() {
         super()
-        this.state = {}
+        this.state = {  // state alwas will be object
+            answer: "Yes"
+        }
     }
-    
+
     render() {
         return (
             <div>
-                <h1>Is state important to know?</h1>
+                <h1>Is state important to know? {this.state.answer}</h1>
+                <ChildComponent answer={this.state.answer} />
             </div>
         )
     }
