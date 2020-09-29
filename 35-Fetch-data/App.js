274bdb5ed41816832d38f9697f079410d8185386
aicoding-girl <erita13@gmail.com>
2020-09-29 11:08:46 +0300

    fetch explained

--- 35-Fetch-data/App.js
+++ 35-Fetch-data/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 
+// https://swapi.dev
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
 class App extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            loading: false,
+            character: {
+
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true })
+        fetch("https://swapi.dev/api/people/1/")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
     }
 
     render() {
+        const text = this.state.loading ? "Loading ..." : this.state.character.name
         return (
             <div>
-
+                <p>{text}</p>
             </div>
         )
     }
