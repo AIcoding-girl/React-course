e08600e04ffc63a2dc40a0efb0905f9e017545aa
aicoding-girl <erita13@gmail.com>
2020-09-29 09:30:24 +0300

    setup for practice

---
+++ 33-Conditional-practice/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+/*
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it
+2. Have state keep track of whether the user is logged in or out
+3. Add button that logs the user in/out
+4. Display text that says "Logged in" if the user is logged in, or "Logged out"
+*/
+
+function App() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
