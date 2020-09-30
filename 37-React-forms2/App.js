1178d57955140dc8ad7846dc6034708306009124
aicoding-girl <erita13@gmail.com>
2020-09-30 14:16:06 +0300

    forms explained

--- 37-React-forms2/App.js
+++ 37-React-forms2/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 
 // https://reactjs.org/docs/forms.html
+// Formik library for writing React forms
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isChecked: false,
+            gender: ""
         }
         this.handleChange = this.handleChange.bind(this)
     }
@@ -18,27 +21,26 @@ class App extends Component {
             [name]: value
         })
     }
-}
 
-render() {
-    return (
-        <div>
-            <form>
-                <input
-                    type="name"
-                    value={this.state.firstName}
-                    name="firstName"
-                    placeholder="First name"
-                    onChange={this.handleChange} />
-                <br />
-                <input
-                    type="name"
-                    value={this.state.lastName}
-                    name="lastName"
-                    placeholder="Last name"
-                    onChange={this.handleChange} />
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="name"
+                        value={this.state.firstName}
+                        name="firstName"
+                        placeholder="First name"
+                        onChange={this.handleChange} />
+                    <br />
+                    <input
+                        type="name"
+                        value={this.state.lastName}
+                        name="lastName"
+                        placeholder="Last name"
+                        onChange={this.handleChange} />
 
-                {/* 
+                    {/* 
                         Other useful form elements:
                             <textarea />
                             <input type="checkbox" />
@@ -46,23 +48,59 @@ render() {
                             <select> and <option> elements
                         */}
 
-                <textarea value={"Some text"}
-                    onChange={this.handleChange}
-                />
-
-                <label>
-                    <input
-                        type="checkbox"
-                        name="isChecked"
-                        checked={this.state.isChecked}
+                    <textarea value={"Some text"}
                         onChange={this.handleChange}
-                    /> Is checked?
-                    </label>
+                    />
 
-            </form>
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isChecked"
+                            checked={this.state.isChecked}
+                            onChange={this.handleChange}
+                        /> Is checked?
+                </label>
 
-        </div>
-    )
-}
+                    {/* radio button is using properties from checkbox and input type="text" - `value` and checked */}
 
-export default App
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            checked={this.state.gender === "male"}
+                            onChange={this.handleChange}
+                        /> Male
+                </label>
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            checked={this.state.gender = "female"}
+                            onChange={this.handleChange}
+                        /> Female
+                </label>
+
+                    <label>Favourite color:</label>
+                    <select
+                        value={this.state.favouriteColor}
+                        onChange={this.handleChange}
+                        name="favouriteColor"
+                    >
+                        <option value="red">Red</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                    </select>
+
+                    <h2>You are a {this.state.gender}</h2>
+                    <h2>Your favourite color is {this.state.favouriteColor}</h2>
+
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+
+    export default App
