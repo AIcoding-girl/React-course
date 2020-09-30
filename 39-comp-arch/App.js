7dd86ea9bf58616eb0f51769a819b8bb2ecd5fb8
aicoding-girl <erita13@gmail.com>
2020-09-30 21:47:58 +0300

    code organized

---
+++ 39-comp-arch/App.js
@@ -0,0 +1,12 @@
+// https://medium.com/@pramonowang/advanced-react-component-patterns-dumb-component-and-smart-component-4cb50fa63aa9
+
+import React, { Component } from 'react'
+import Form from './FormContainer'
+
+function App() {
+    return (
+        <Form />
+    )
+}
+
+export default App
