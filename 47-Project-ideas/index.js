d9dac26f66c03bb660c7c323504ef385e1e84fac
aicoding-girl <erita13@gmail.com>
2020-10-01 13:46:27 +0300

    ideas shared

---
+++ 47-Project-ideas/index.js
@@ -0,0 +1,7 @@
+/*
+https://medium.com/free-code-camp/every-time-you-build-a-to-do-list-app-a-puppy-dies-here-are-some-better-project-ideas-279d4055f77
+
+https://www.freecodecamp.org/news/want-to-build-something-fun-heres-a-list-of-sample-web-app-ideas-b991bce0ed9a/
+
+https://medium.com/better-programming/summer-is-over-you-should-be-coding-heres-yet-another-list-of-exciting-ideas-to-build-a95d7704d36d
+*/
