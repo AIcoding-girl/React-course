93dd88b710e6142fcacaa3c374322b0d4613fb0c
aicoding-girl <erita13@gmail.com>
2020-10-01 11:44:39 +0300

    hooks in short

---
+++ 42-React-Hooks/Hooks.js
@@ -0,0 +1,11 @@
+/*
+React Hooks
+
+"Hook into" state and lifecycle methods of components without using classes
+Only use functional components across the board
+Improve readability and organization of components
+
+useState()
+useEffect()
+
+*/
