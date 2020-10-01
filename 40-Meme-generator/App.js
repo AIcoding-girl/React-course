a560ba644ee3f8e109e0089c322d2c24132207ce
aicoding-girl <erita13@gmail.com>
2020-10-01 09:27:56 +0300

    project setup

---
+++ 40-Meme-generator/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Step 1: create 2 new components - Header and NameGenerator
+// Header will only display things
+// Meme generator will be calling to an API and holding on to data
+
+function App() {
+    return (
+        <h1>App</h1>
+    )
+}
+
+export default App
