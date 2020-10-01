39aea701d94a7a533d5f66bb56acc614cfd4f7ca
aicoding-girl <erita13@gmail.com>
2020-10-01 11:55:17 +0300

    hooks example

---
+++ 43-useState-part1/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+    const [answer, ??? ] = useState("Yes")  // returns an array ["", f()]
+
+    return (
+        <div>
+            <h1>Is state important to know? {answer}</h1>
+        </div>
+    )
+}
+
+export default App
